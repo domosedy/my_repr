38b2880064c14583b7da2bbe3a7c81e420d9edf8
domosedy <52038168+domosedy@users.noreply.github.com>
2021-04-23 06:40:42 +0300

    Update main.py
    As

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.config['SECRET_KEY'] = 'chupakabra'
 
 file = "db/blogs.sqlite"
 engine = create_engine(f'sqlite:///{file.strip()}', echo=True)
-engine = create_engine(f'postgresql://localhost/{file.strip()}', echo=True)
+engine = create_engine(f'postgresql://0.0.0.0/{file.strip()}', echo=True)
 
 Session = sessionmaker(bind=engine)
 
